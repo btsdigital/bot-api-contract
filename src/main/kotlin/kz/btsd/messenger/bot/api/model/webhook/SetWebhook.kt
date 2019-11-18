d4d7cb4b9283fa9275433fbd8ec2aade49691b42
mustafin <mustafinmurat@gmail.com>
2019-11-18 12:33:45 +0600

    Add url validator size message

--- src/main/kotlin/kz/btsd/messenger/bot/api/model/webhook/SetWebhook.kt
+++ src/main/kotlin/kz/btsd/messenger/bot/api/model/webhook/SetWebhook.kt
@@ -9,6 +9,6 @@ data class SetWebhook(
     @ApiModelProperty(
         notes = "Webhook url"
     )
-    @get:Size(max = 200)
+    @get:Size(max = 200, message = "Size of url should be <= {max}")
     val url: String
 )
